f87cc11239fb1e87e3920b6cd23b1bbac162adb7
Maks <mr.f.f.n.v@gmail.com>
2024-03-15 15:15:30 +0300

    jpa

--- src/main/java/com/foodordering/Taco.java
+++ src/main/java/com/foodordering/Taco.java
@@ -1,5 +1,10 @@
 package com.foodordering;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
@@ -7,14 +12,15 @@ import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date createdAt = new Date();
 
@@ -24,6 +30,11 @@ public class Taco {
 
 
     @Size(min=1, message="Необходимо выбрать как минимум один ингредиент")
+    @ManyToMany()
     private List<Ingredient> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 
 }
--- src/main/java/com/foodordering/TacoOrder.java
+++ src/main/java/com/foodordering/TacoOrder.java
@@ -1,5 +1,11 @@
 package com.foodordering;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -9,16 +15,16 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import java.util.List;
 import java.util.ArrayList;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@Entity
 public class TacoOrder implements Serializable {
 
     private final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
@@ -48,6 +54,7 @@ public class TacoOrder implements Serializable {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
