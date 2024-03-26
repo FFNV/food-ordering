2381d2e7d89302b1f850019e3bc69764901c46e3
Maks <mr.f.f.n.v@gmail.com>
2024-03-26 19:49:12 +0300

    Security

--- src/main/java/com/foodordering/data/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodordering.data;
-
-import com.foodordering.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IngredientRepository
-        extends CrudRepository<Ingredient, String> {
-
-}
--- src/main/java/com/foodordering/data/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodordering.data;
-
-import com.foodordering.TacoOrder;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository
-        extends CrudRepository<TacoOrder, String> {
-
-}
